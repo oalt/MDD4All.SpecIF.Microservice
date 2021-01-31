70b9fff1626cbe052cd95663c514305578dc07f1
oalt <oa@lacheweg21.de>
2021-01-31 12:27:54 +0100

    Add manipulation extension for project description

---
+++ src/MDD4All.SpecIF.DataModels.Manipulation/ProjectDescriptorManipulationExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MDD4All.SpecIF.DataModels.Manipulation
+{
+    public static class ProjectDescriptorManipulationExtensions
+    {
+        public static string GetTitleValue(this ProjectDescriptor projectDescriptor, string language = "de")
+        {
+            string result = "";
+
+            if(projectDescriptor.Title is string)
+            {
+                result = projectDescriptor.Title as string;
+            }
+            // TODO Language value
+
+            return result;
+        }
+    }
+}
