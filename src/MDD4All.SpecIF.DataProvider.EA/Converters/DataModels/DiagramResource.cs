a0d60a331bab917e0b5b9298df968d9b67d63188
oalt <oa@lacheweg21.de>
2021-04-27 12:53:02 +0200

    Adding DiagramResource

---
+++ src/MDD4All.SpecIF.DataProvider.EA/Converters/DataModels/DiagramResource.cs
@@ -0,0 +1,16 @@
+﻿using MDD4All.SpecIF.DataModels;
+using System.Collections.Generic;
+
+namespace MDD4All.SpecIF.DataProvider.EA.Converters.DataModels
+{
+    public class DiagramResource
+    {
+        public Key Key { get; set; }
+
+        public string EaGUID { get; set; }
+
+        public Resource Resource { get; set; }
+
+        public List<Statement> ImplicitDiagramStatements { get; set; } = new List<Statement>();
+    }
+}
